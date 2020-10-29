23bc97c3d04c83285adc02b8382813f64252d28c
MuhammadSaqibHanif <ssaqibhanif777@gmail.com>
2020-10-29 18:37:42 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>REACT</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
